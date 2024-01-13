7be1c8108bc29ea9c419bf7b923d0c7e5204ea26
praveen kumar mahto <mahtoji349@gmail.com>
2024-01-13 10:56:02 +0530

    blogs updated

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -23,11 +23,23 @@ public class BlogService {
 
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        User user = userRepository1.findById(userId).get();
+        Blog blog = new Blog();
+        blog.setTitle(title);
+        blog.setContent(content);
+        blog.setPubDate(new Date());
+        blog.setUser(user);
+        user.getBlogList().add(blog);
+        userRepository1.save(user);
+        return blog;
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
+        blogRepository1.deleteById(blogId);
+//        Blog blog = blogRepository1.findById(blogId).get();
+//        User user = blog.getUser();
+//        user.getBlogList().remove(blog);
+//        blogRepository1.deleteById(blogId);
     }
 }
